dd0bde78973e1d169e6c6ba3c4a88b8c3950ddf3
artzha <arthurz@cs.utexas.edu>
2025-06-06 16:20:57 -0500

    Increased support grid size to match paper

--- cotracker/predictor.py
+++ cotracker/predictor.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 from cotracker.models.core.model_utils import smart_cat, get_points_on_a_grid
 from cotracker.models.build_cotracker import build_cotracker
 
-CONF_THR = 0.5 # threshold for confidence in online mode, used in CoTrackerOnlinePredictor
+CONF_THR = 0.9 # threshold for confidence in online mode, used in CoTrackerOnlinePredictor
 
 class CoTrackerPredictor(torch.nn.Module):
     def __init__(
@@ -22,7 +22,7 @@ class CoTrackerPredictor(torch.nn.Module):
     ):
         super().__init__()
         self.v2 = v2
-        self.support_grid_size = 6
+        self.support_grid_size = 8 # defaults to 6 (Paper found best with 8)
         model = build_cotracker(
             checkpoint,
             v2=v2,
